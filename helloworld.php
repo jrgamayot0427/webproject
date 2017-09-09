7e30102c0c5e2e2d316e6bad5f3b213f69b80431
Jomari Eneldas <jomari.eneldas7@gmail.com>
2017-09-09 14:27:30 +0800

    etona

--- helloworld.php
+++ helloworld.php
@@ -1,8 +1,27 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>hello world app</title>
+<title>Hello World Git</title>
+
+<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+<meta name="author" content="Team Bah" />
+<meta name="description" content="The description of website" />
+
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
+<link href="bootstrap/css/bootstrap-theme.css" rel="stylesheet" />
+<link href="assets/css/mybootstrap.css" rel="stylesheet" />
+
+<script src="bootstrap/js/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
 </head>
 <body>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 header">
+			<h1>Hello Bootstrap!</h1>
+		</div>
+	</div>
 <?php
 echo 'hello world';
 
